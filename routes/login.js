0c8a0ecf1501d8e146b06ab521ee4e988d81dc60
Jack1065 <115672450+Jack1065@users.noreply.github.com>
2024-11-20 14:26:16 -0600

    Login Verification
    tested API endpoints via postman. Now able to verify valid login.

--- routes/login.js
+++ routes/login.js
@@ -1,15 +1,45 @@
 //router for logins
+const { Neurosity } = require('@neurosity/sdk');
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const router = express.Router();
+const { processLogin, main } = require('../index');
 
 
-router.get("/", (req,res)=> {
-    res.sendFile(path.join(__dirname,'../headset/controls.html'));
-})
-router.get("/new", (req,res)=> {
-    res.send("New Login");
-})
 
 
-module.exports = router
+router.post("/", async(req,res) => {
+   
+    console.log("test")
+   // res.sendFile(path.join(__dirname,'../headset/controls.html'));
+   try{
+    const {username,password,deviceID} = req.body;
+
+    if (!processLogin(username,password,deviceID)){
+        console.log('Login Error')
+        return res.status(400).send("Invalid Login");
+    }
+
+    const neurosity = new Neurosity({
+        deviceID
+    })
+
+    const loggedIn= await main(username, password, neurosity);
+    if(loggedIn){
+        res.sendFile(path.join(__dirname, '../headset/controls.html'));
+    }
+    else{
+        console.log("Login Failed");
+        res.status(401).send('Failed to login');
+    }
+   }
+   catch(error){
+    console.log(error);
+    res.status(500).send('error occured');
+   }
+
+
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//establish server connection
 const express = require("express");
 const http = require("http");
 const path = require("path");
@@ -5,13 +6,18 @@ const app = express();
 const server = http.createServer(app);
 const port = 3000;
 
-
+//specify path dependencies
+app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
 app.use(express.static(path.join(__dirname + '/headset')));
 
+
+// Serve login.html at root level
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/headset/login.html')); // Serve login.html
+    res.sendFile(path.join(__dirname, '/headset/login.html')); 
 });
 
+//establish routes specify login route
 const loginRouter = require('./routes/login')
 
 
