bc86a1da75f55c50f9bd527ec15470481ba1bbfc
CooperRecob <crecob1@gmail.com>
2024-11-26 19:32:23 -0600

    adding Chris's changes manually since the merge kept failing

--- www/cooper/public/js/script.js
+++ www/cooper/public/js/script.js
@@ -5,16 +5,30 @@ function sendCommand(command) {
     console.log(`Sent command: ${command}`);
 }
 
-function connect() {
-    sendCommand('command')
+async function connect() {
+    const levelDiv = document.getElementById('connect');
+    
+    try {
+        const diditconnect = await sendCommand('command');
+        levelDiv.style.backgroundColor = '#4caf50';  // Green for success
+    } catch (error) {
+        console.error('Error occurred while connecting:', error);
+        levelDiv.style.backgroundColor = '#f44336';  // Red for error
+    }
 }
+
+function sleep() {
+    ms = 3000
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 
 // rc a b c d
 // a: left/right
 // b: forward/backward
 // c: up/down
 // d: yaw
-// space: emergeny stop
+// space: emergency stop
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
         case 'w':
@@ -49,6 +63,22 @@ document.addEventListener('keydown', (event) => {
             break;
         case '`':
             sendCommand('emergency');
+            break;
+        case 'ArrowLeft':
+            sendCommand('flip l');
+            sleep();
+            break;
+        case 'ArrowRight':
+            sendCommand('flip r');
+            sleep();
+            break;
+        case 'f':
+            sendCommand('flip f');
+            sleep();
+            break;
+        case 'b':
+            sendCommand('flip b');
+            sleep();
             break;
     }
 });
